7d3cfc9a68c09cdf4115284514a269bae23dfcd6
ingalls <nick@mapbox.com>
2017-08-14 21:56:56 -0400

    Add post dedupe and framework tests

---
+++ test/post.dedupe.test.js
@@ -0,0 +1,42 @@
+const post = require('../lib/post/dedupe').post;
+const test = require('tape');
+
+test('Post: Dedupe', (t) => {
+    t.equals(post(), undefined);
+
+    t.equals(post({
+        properties: undefined
+    }), undefined);
+
+    t.equals(post({
+        properties: {
+            'carmen:addressnumber': undefined
+        }
+    }), undefined);
+
+    t.equals(post({
+        properties: {
+            'carmen:addressnumber': []
+        }
+    }), undefined);
+
+    t.equals(post({
+        properties: {
+            'carmen:addressnumber': [ 1 ]
+        }
+    }), undefined);
+
+    t.equals(post({
+        properties: {
+            'carmen:addressnumber': [ 1, 2, 3, 4 ]
+        }
+    }), undefined);
+
+    t.equals(post({
+        properties: {
+            'carmen:addressnumber': [ 1, 2, 3, 4, 1, 2, 3, 4 ]
+        }
+    }), undefined);
+
+    t.end();
+});
