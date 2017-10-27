77ccdfd1220a787f49d365998107f9b31099a3ad
Kai <kai@mapbox.com>
2017-10-27 14:21:16 -0400

    get cardinal test running

--- test/map.test.js
+++ test/map.test.js
@@ -45,13 +45,13 @@ test('map - db error', (t) => {
 
 // new test
 // runs map.js
-test.skip('map - cardinal clustering', (t) => {
+test('map - cardinal clustering', (t) => {
     worker({
         'in-address': './test/fixtures/cardinal-address.geojson',
         'in-network': './test/fixtures/cardinal-network.geojson',
         output: '/tmp/itp.geojson',
         debug: true,
-        db: 'pt_test_cardinal'
+        db: 'pt_test'
     }, (err, res) => {
         t.error(err);
 
@@ -64,18 +64,7 @@ test.skip('map - cardinal clustering', (t) => {
 
             feat = JSON.parse(line);
 
-            //TODO PT2ITP is not deterministic and subsequent runs can change the output value based on unordered operations.
-            //      For these tests to function properly a full deterministic quest will have to be pursued. We should do this
-            //if (feat.properties['carmen:text'] === 'Muscat Street') checkFixture(feat, 'muscat-st');
-            //if (feat.properties['carmen:text'] === 'Park Road,Parsi Road') checkFixture(feat, 'park-rd');
-            //if (feat.properties['carmen:text'] === 'Teck Lim Road') checkFixture(feat, 'teck-lim');
-            //if (feat.properties['carmen:text'] === 'Jalan Kelempong') checkFixture(feat, 'jalam-kelempong');
-            //if (feat.properties['carmen:text'] === 'Tomlinson Road,Tomlison Road') checkFixture(feat, 'tomlinson');
-            //if (feat.properties['carmen:text'] === 'Jalan Sejarah') checkFixture(feat, 'jalan-sejrah');
-            //if (feat.properties['carmen:text'] === 'Changi South Street 3') checkFixture(feat, 'changi');
-            //if (feat.properties['carmen:text'] === 'Lorong 21a Geylang') checkFixture(feat, 'lorong');
-            //if (feat.properties['carmen:text'] === 'Ang Mo Kio Industrial Park 3') checkFixture(feat, 'ang-mo');
-            //if (feat.properties['carmen:text'] === 'De Souza Avenue') checkFixture(feat, 'de-souza');
+            // things to check
         });
 
         rl.on('error', t.error);
@@ -94,84 +83,24 @@ test.skip('map - cardinal clustering', (t) => {
             t.ok(res.id);
             delete res.id;
 
-            let known = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./fixtures/sg-${fixture}`)));
+            let known = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./fixtures/cardinal-${fixture}`)));
 
             t.deepEquals(res, known);
 
             if (process.env.UPDATE) {
                 t.fail();
-                fs.writeFileSync(path.resolve(__dirname, `./fixtures/sg-${fixture}`), JSON.stringify(res, null, 4));
+                fs.writeFileSync(path.resolve(__dirname, `./fixtures/cardinal-${fixture}`), JSON.stringify(res, null, 4));
             }
         }
     });
 });
 
-test('map - good run', (t) => {
-    worker({
-        'in-address': './test/fixtures/sg-address.geojson',
-        'in-network': './test/fixtures/sg-network.geojson',
-        output: '/tmp/itp.geojson',
-        debug: true,
-        db: 'pt_test_good'
-    }, (err, res) => {
-        t.error(err);
 
-        rl = ReadLine.createInterface({
-            input: fs.createReadStream('/tmp/itp.geojson')
-        });
-
-        rl.on('line', (line) => {
-            if (!line) return;
-
-            feat = JSON.parse(line);
-
-            //TODO PT2ITP is not deterministic and subsequent runs can change the output value based on unordered operations.
-            //      For these tests to function properly a full deterministic quest will have to be pursued. We should do this
-            //if (feat.properties['carmen:text'] === 'Muscat Street') checkFixture(feat, 'muscat-st');
-            //if (feat.properties['carmen:text'] === 'Park Road,Parsi Road') checkFixture(feat, 'park-rd');
-            //if (feat.properties['carmen:text'] === 'Teck Lim Road') checkFixture(feat, 'teck-lim');
-            //if (feat.properties['carmen:text'] === 'Jalan Kelempong') checkFixture(feat, 'jalam-kelempong');
-            //if (feat.properties['carmen:text'] === 'Tomlinson Road,Tomlison Road') checkFixture(feat, 'tomlinson');
-            //if (feat.properties['carmen:text'] === 'Jalan Sejarah') checkFixture(feat, 'jalan-sejrah');
-            //if (feat.properties['carmen:text'] === 'Changi South Street 3') checkFixture(feat, 'changi');
-            //if (feat.properties['carmen:text'] === 'Lorong 21a Geylang') checkFixture(feat, 'lorong');
-            //if (feat.properties['carmen:text'] === 'Ang Mo Kio Industrial Park 3') checkFixture(feat, 'ang-mo');
-            //if (feat.properties['carmen:text'] === 'De Souza Avenue') checkFixture(feat, 'de-souza');
-        });
-
-        rl.on('error', t.error);
-
-        rl.on('close', () => {
-            fs.unlinkSync('/tmp/itp.geojson');
-            t.end();
-        });
-
-        /**
-         * Standard Fixture compare/update
-         * @param {Object} res returned result
-         * @param {string} fixture Path to expected result file
-         */
-        function checkFixture(res, fixture) {
-            t.ok(res.id);
-            delete res.id;
-
-            let known = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./fixtures/sg-${fixture}`)));
-
-            t.deepEquals(res, known);
-
-            if (process.env.UPDATE) {
-                t.fail();
-                fs.writeFileSync(path.resolve(__dirname, `./fixtures/sg-${fixture}`), JSON.stringify(res, null, 4));
-            }
-        }
-    });
-});
-
-test.skip('drop cardinal database', (t) => {
+test('drop cardinal database', (t) => {
     let pool = new pg.Pool({
         max: 10,
         user: 'postgres',
-        database: 'pt_test_cardinal',
+        database: 'pt_test',
         idleTimeoutMillis: 30000
     });
 
@@ -189,11 +118,74 @@ test.skip('drop cardinal database', (t) => {
     });
 });
 
-test('drop good-run database', (t) => {
+test.skip('map - good run', (t) => {
+    worker({
+        'in-address': './test/fixtures/sg-address.geojson',
+        'in-network': './test/fixtures/sg-network.geojson',
+        output: '/tmp/itp.geojson',
+        debug: true,
+        db: 'pt_test'
+    }, (err, res) => {
+        t.error(err);
+
+        rl = ReadLine.createInterface({
+            input: fs.createReadStream('/tmp/itp.geojson')
+        });
+
+        rl.on('line', (line) => {
+            if (!line) return;
+
+            feat = JSON.parse(line);
+
+            console.log('test feat:', feat.properties);
+
+            //TODO PT2ITP is not deterministic and subsequent runs can change the output value based on unordered operations.
+            //      For these tests to function properly a full deterministic quest will have to be pursued. We should do this
+            //if (feat.properties['carmen:text'] === 'Muscat Street') checkFixture(feat, 'muscat-st');
+            //if (feat.properties['carmen:text'] === 'Park Road,Parsi Road') checkFixture(feat, 'park-rd');
+            //if (feat.properties['carmen:text'] === 'Teck Lim Road') checkFixture(feat, 'teck-lim');
+            //if (feat.properties['carmen:text'] === 'Jalan Kelempong') checkFixture(feat, 'jalam-kelempong');
+            //if (feat.properties['carmen:text'] === 'Tomlinson Road,Tomlison Road') checkFixture(feat, 'tomlinson');
+            //if (feat.properties['carmen:text'] === 'Jalan Sejarah') checkFixture(feat, 'jalan-sejrah');
+            //if (feat.properties['carmen:text'] === 'Changi South Street 3') checkFixture(feat, 'changi');
+            //if (feat.properties['carmen:text'] === 'Lorong 21a Geylang') checkFixture(feat, 'lorong');
+            //if (feat.properties['carmen:text'] === 'Ang Mo Kio Industrial Park 3') checkFixture(feat, 'ang-mo');
+            //if (feat.properties['carmen:text'] === 'De Souza Avenue') checkFixture(feat, 'de-souza');
+        });
+
+        rl.on('error', t.error);
+
+        rl.on('close', () => {
+            fs.unlinkSync('/tmp/itp.geojson');
+            t.end();
+        });
+
+        /**
+         * Standard Fixture compare/update
+         * @param {Object} res returned result
+         * @param {string} fixture Path to expected result file
+         */
+        function checkFixture(res, fixture) {
+            t.ok(res.id);
+            delete res.id;
+
+            let known = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./fixtures/sg-${fixture}`)));
+
+            t.deepEquals(res, known);
+
+            if (process.env.UPDATE) {
+                t.fail();
+                fs.writeFileSync(path.resolve(__dirname, `./fixtures/sg-${fixture}`), JSON.stringify(res, null, 4));
+            }
+        }
+    });
+});
+
+test.skip('drop good-run database', (t) => {
     let pool = new pg.Pool({
         max: 10,
         user: 'postgres',
-        database: 'pt_test_good',
+        database: 'pt_test',
         idleTimeoutMillis: 30000
     });
 
