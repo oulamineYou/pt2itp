ccb2f6da81e688594522590c9310290d09d9c5e9
Bob Lannon <bob.lannon@mapbox.com>
2017-11-07 22:49:17 +0000

    expected frequency distribution shouldn't include ngrams across different text rows

--- test/fixtures/freqDist.js
+++ test/fixtures/freqDist.js
@@ -1,11 +1,9 @@
-module.exports.freqDist = [ 
-{ w1: 'akoko', w2: 'street', frequency: 1, likelihoodRatio: 6.701994141683237 },
-{ w1: 'street', w2: 'wong', frequency: 1, likelihoodRatio: 6.701994141683237 }, 
-{ w1: 'wong', w2: 'ho', frequency: 1, likelihoodRatio: 6.701994141683237 }, 
-{ w1: 'ho', w2: 'lane', frequency: 1, likelihoodRatio: 6.701994141683237 }, 
-{ w1: 'lane', w2: 'pier', frequency: 1, likelihoodRatio: 6.701994141683237 }, 
-{ w1: 'pier', w2: '1', frequency: 1, likelihoodRatio: 6.701994141683237 }, 
-{ w1: '1', w2: 'main', frequency: 1, likelihoodRatio: 6.701994141683237 }, 
-{ w1: 'main', w2: 'st', frequency: 1, likelihoodRatio: 3.9294054194434587 },
-{ w1: 'st', w2: 'fake', frequency: 1, likelihoodRatio: 3.9294054194434587 }, 
-{ w1: 'fake', w2: 'st', frequency: 1, likelihoodRatio: 3.9294054194434587 } ];
+module.exports.freqDist = [
+  { w1: 'akoko', w2: 'street', frequency: 1, likelihoodRatio: 6.701994141683237 },
+  { w1: 'wong', w2: 'ho', frequency: 1, likelihoodRatio: 6.701994141683237 },
+  { w1: 'ho', w2: 'lane', frequency: 1, likelihoodRatio: 6.701994141683237 },
+  { w1: 'pier', w2: '1', frequency: 1, likelihoodRatio: 6.701994141683237 },
+  { w1: 'main', w2: 'st', frequency:1, likelihoodRatio: 3.9294054194434587 },
+  { w1: 'fake', w2: 'st', frequency: 1, likelihoodRatio: 3.9294054194434587 }
+]
+
