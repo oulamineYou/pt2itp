ccae2c044f363616b487ea9f81041072b9d8edb9
ingalls <ingalls@protonmail.com>
2017-11-29 21:34:22 -0500

    Add post file and more tests

--- test/post.centre.test.js
+++ test/post.centre.test.js
@@ -12,7 +12,34 @@ test('Post: Centre', (t) => {
                 "coordinates": [ [ -68.66264104843138, 45.46536797816513 ], [ -68.64938020706177, 45.46925051979969 ] ]
             }]
         }
-     }).properties['carmen:center'], []);
+     }).properties['carmen:center'], [ -68.64938020706177, 45.46925051979969 ]);
+
+     t.deepEquals(post({
+        type: 'Feature',
+        properties: {},
+        geometry: {
+            type: 'GeometryCollection',
+            geometries: [{
+                "type": "LineString",
+                "coordinates": [ [ -68.66264104843138, 45.46536797816513 ], [ -68.64938020706177, 45.46925051979969 ] ]
+            },{
+                "type": "MultiPoint",
+                "coordinates": [ [ -68.662641048431, 45.46536797816513 ], [ -68.649380207061, 45.46925051979969 ] ]
+            }]
+        }
+     }).properties['carmen:center'], [ -68.649380207061, 45.46925051979969 ]);
+
+     t.deepEquals(post({
+        type: 'Feature',
+        properties: {},
+        geometry: {
+            type: 'GeometryCollection',
+            geometries: [{
+                "type": "MultiPoint",
+                "coordinates": [ [ -68.662641048431, 45.46536797816513 ], [ -68.649380207061, 45.46925051979969 ] ]
+            }]
+        }
+     }).properties['carmen:center'], [ -68.649380207061, 45.46925051979969 ]);
 
     t.end();
 });
