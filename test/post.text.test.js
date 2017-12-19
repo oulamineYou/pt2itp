bf7c6c6e3c9336ae7e369f4411b6a03defbda35e
ingalls <ingalls@protonmail.com>
2017-12-18 16:35:06 -0800

    Update sort order

--- test/post.text.test.js
+++ test/post.text.test.js
@@ -1,6 +1,10 @@
 const post = require('../lib/post/text').post;
 const test = require('tape');
 
+const args = {
+    label: require('../lib/label/titlecase')()
+}
+
 test('Post: Dedupe', (t) => {
     t.deepEquals(post(), undefined, 'return unprocessable 1');
 
@@ -15,7 +19,7 @@ test('Post: Dedupe', (t) => {
             'carmen:text': [{ display: 'Main Street', tokenized: 'main st' }],
             'carmen:text_xx': [{ display: 'Spring Rd', tokenized: 'spring rd' }]
         }
-    }), {
+    }, args), {
         properties: {
             'carmen:text': 'Main Street',
             'carmen:text_xx': 'Spring Rd'
@@ -24,10 +28,17 @@ test('Post: Dedupe', (t) => {
 
     t.deepEquals(post({
         properties: {
-            'carmen:text': 'Main Street,Some Other St,Main Street',
-            'carmen:text_xx': 'Spring Rd,Spring Rd'
+            'carmen:text': [
+                { display: 'Main Street', tokenized: 'main st' },
+                { display: 'Some Other St', tokenized: 'some other st' },
+                { display: 'Main Street', tokenized: 'main st' },
+            ],
+            'carmen:text_xx': [
+                { display: 'Spring Rd', tokenized: 'spring rd' },
+                { display: 'Spring Rd', tokenized: 'spring rd' }
+            ]
         }
-    }), {
+    }, args), {
         properties: {
             'carmen:text': 'Main Street,Some Other St',
             'carmen:text_xx': 'Spring Rd'
@@ -36,43 +47,53 @@ test('Post: Dedupe', (t) => {
 
     t.deepEquals(post({
         properties: {
-            'carmen:text': 'Main St,Some Other St,Main Street',
-            'carmen:text_xx': 'Spring Road,Spring Rd',
-            'carmen:text_es': 'Pta Something,Spring Road,Puerta Something'
+            'carmen:text': [
+                { display: 'Main St', tokenized: 'main st' },
+                { display: 'Some Other St', tokenized: 'some other st' },
+                { display: 'Main Street', tokenized: 'main st' }
+            ],
+            'carmen:text_xx': [
+                { display: 'Spring Road', tokenized: 'spring rd' },
+                { display: 'Spring Rd', tokenized: 'spring rd' }
+            ],
+            'carmen:text_es': [
+                { display: 'Pta Something', tokenized: 'pta something' },
+                { display: 'Spring Road', tokenized: 'spring rd' },
+                { display: 'Puerta Something', tokenized: 'puerta something' }
+            ]
         }
-    }, { tokens: 'en' }), {
+    }, args), {
         properties: {
             'carmen:text': 'Main Street,Some Other St',
             'carmen:text_xx': 'Spring Road',
-            'carmen:text_es': 'Pta Something,Spring Road,Puerta Something'
+            'carmen:text_es': 'Pta Something,Puerta Something,Spring Road'
 
         }
     }, 'dedupe tokens, single language');
 
     t.deepEquals(post({
         properties: {
-            'carmen:text': 'Main St,Some Other St,Main Street',
-            'carmen:text_xx': 'Spring Road,Spring Rd',
-            'carmen:text_es': 'Pta Something,Spring Road,Puerta Something'
+            'carmen:text': [
+                { display: '204 Haywood Rd', tokenized: '204 haywood rd' },
+                { display: '201 Haywood Rd', tokenized: '201 haywood rd' },
+                { display: '202 Haywood Rd', tokenized: '202 haywood rd' },
+                { display: '203 Haywood Rd', tokenized: '203 haywood rd' },
+                { display: '208 Haywood Rd', tokenized: '208 haywood rd' },
+                { display: '209 Haywood Rd', tokenized: '209 haywood rd' },
+                { display: '210 Haywood Rd', tokenized: '210 haywood rd' },
+                { display: '211 Haywood Rd', tokenized: '211 haywood rd' },
+                { display: '212 Haywood Rd', tokenized: '212 haywood rd' },
+                { display: '213 Haywood Rd', tokenized: '213 haywood rd' },
+                { display: '214 Haywood Rd', tokenized: '214 haywood rd' },
+                { display: '215 Haywood Rd', tokenized: '215 haywood rd' },
+                { display: '216 Haywood Rd', tokenized: '216 haywood rd' },
+                { display: '217 Haywood Rd', tokenized: '217 haywood rd' },
+                { display: '218 Haywood Rd', tokenized: '218 haywood rd' }
+            ]
         }
-    }, { tokens: ['en', 'es'] }), {
+    }, args), {
         properties: {
-            'carmen:text': 'Main Street,Some Other St',
-            'carmen:text_xx': 'Spring Road',
-            'carmen:text_es': 'Puerta Something,Spring Road'
-
-        }
-    }, 'dedupe tokens, multi language');
-
-    t.deepEquals(post({
-        properties: {
-            'carmen:text': '204 Haywood Rd,201 Haywood Rd,202 Haywood Rd,203 Haywood Rd,208 Haywood Rd,209 Haywood Rd,210 Haywood Rd,211 Haywood Rd,212 Haywood Rd,213 Haywood Rd,214 Haywood Rd,215 Haywood Rd,216 Haywood Rd,217 Haywood Rd,218 Haywood Rd',
-            'carmen:text_xx': '204 Haywood Rd,201 Haywood Rd,202 Haywood Rd,203 Haywood Rd,208 Haywood Rd,209 Haywood Rd,210 Haywood Rd,211 Haywood Rd,212 Haywood Rd,213 Haywood Rd,214 Haywood Rd,215 Haywood Rd,216 Haywood Rd,217 Haywood Rd,218 Haywood Rd'
-        }
-    }, { tokens: ['en', 'es'] }), {
-        properties: {
-            'carmen:text': '204 Haywood Rd,201 Haywood Rd,202 Haywood Rd,203 Haywood Rd,208 Haywood Rd,209 Haywood Rd,210 Haywood Rd,211 Haywood Rd,212 Haywood Rd,213 Haywood Rd',
-            'carmen:text_xx': '204 Haywood Rd,201 Haywood Rd,202 Haywood Rd,203 Haywood Rd,208 Haywood Rd,209 Haywood Rd,210 Haywood Rd,211 Haywood Rd,212 Haywood Rd,213 Haywood Rd'
+            'carmen:text': '201 Haywood Rd,204 Haywood Rd,212 Haywood Rd,213 Haywood Rd,210 Haywood Rd,215 Haywood Rd,216 Haywood Rd,203 Haywood Rd,217 Haywood Rd,202 Haywood Rd',
         }
     }, 'dedupe tokens, excessive synonyms');
 
