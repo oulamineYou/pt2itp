ee2fbdb7c8c6493130fb160c7ea5d760d2115869
Andrea del Rio <adelrio@gmail.com>
2018-01-12 14:43:40 -0800

    typo fix

--- test/test.test.js
+++ test/test.test.js
@@ -63,7 +63,7 @@ test('Run test mode', (t) => {
         .on('end', () => {
             t.equal(csvErrs.length, 2);
             t.equal(csvErrs.filter(ele => ele.query === '5 greeeeeenview rd')[0].error, 'NO RESULTS');
-            t.equal(csvErrs.filter(ele => ele['addr text'] === 'Greeeeeenview Rd')[0].error, 'NAME MISMATCH (SOFT)');
+            t.equal(csvErrs.filter(ele => ele['addr text'] === 'greeeeeenview Rd')[0].error, 'NAME MISMATCH (SOFT)');
             t.end();
         });
     });
