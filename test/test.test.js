d297702f3cf9bfa40bfd1cec7b0b1015306d9aa1
Andrea del Rio <adelrio@gmail.com>
2018-01-18 15:54:39 -0500

    mv test from db to carmn-index query

--- test/test.test.js
+++ test/test.test.js
@@ -8,47 +8,16 @@ const test = require('tape');
 const path = require('path');
 const pg = require('pg');
 
-const database = 'pt_test';
+// const database = 'pt_test';
 const carmenIndex = path.resolve(__dirname, './fixtures/test-ri/index/us_ri-address-both-0d603c2a171017011038-0d603c2a39.mbtiles');
 const output = '/tmp/test-ri.err';
-const config = path.resolve(__dirname, './fixtures/test-ri/carmen-config.json');
-
-const pool = new pg.Pool({
-    max: 10,
-    user: 'postgres',
-    database: database,
-    idleTimeoutMillis: 30000
-});
-
-const index = new Index(pool);
-
-test('Drop/init database', (t) => {
-    index.init((err, res) => {
-        t.error(err);
-        t.end();
-    });
-});
-
-// loads address and network data into postgres
-test('load address and network files', (t) => {
-    worker({
-        'in-address': './test/fixtures/test-ri/address.geojson',
-        'in-network': './test/fixtures/test-ri/network.geojson',
-        output: '/tmp/itp.geojson',
-        debug: true,
-        db: database,
-        tokens: 'en'
-    }, (err, res) => {
-        t.error(err);
-        t.end();
-    });
-});
+const config = path.resolve(__dirname, './fixtures/test-ri/carmen-config2.json');
 
 test('Run test mode', (t) => {
-    exec(`${__dirname}/../index.js test - | carmen-index --config=${config} --index=${carmenIndex}`);
-    exec(`${__dirname}/../index.js test --index ${carmenIndex} --database ${database} --output ${output} --config ${config}`, (err, stdout, stderr) => {
+    exec(`${__dirname}/../index.js test - | carmen-index --config=${config} --index=${carmenIndex}`, (err, stdout, stderr) => {
         t.test('Return correct error messages in csv', (t) => {
             let csvErrs = [];
+            let queryResults;
 
             csv.fromPath(output, {headers: true})
             .on('data', (data) => {
@@ -58,20 +27,13 @@ test('Run test mode', (t) => {
                 t.equal(csvErrs.length, 2);
                 t.equal(csvErrs.filter(ele => ele.query === '5 greeeeeenview rd')[0].error, 'NO RESULTS');
                 t.equal(csvErrs.filter(ele => ele['addr text'] === 'greeeeeenview')[0].error, 'NAME MISMATCH (SOFT)');
+                exec(`carmen --query "5 Greenview Rd" /tmp/ri.mbtiles | grep "1.00 5 Greenview Rd" | tr -d '\n'`, (err, res) => {
+                    t.equal(res, "- 1.00 5 Greenview Rd,  (address.1216774020)")
+                });
+
                 t.end();
             });
         });
     });
-});
 
-test('Drop/init database', (t) => {
-    index.init((err, res) => {
-        t.error(err);
-        t.end();
-    });
-});
-
-test('end connection', (t) => {
-    pool.end();
-    t.end();
 });
