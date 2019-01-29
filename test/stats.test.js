e754c1f5da92552e148ee290cbf76962146e507a
ingalls <ingalls@protonmail.com>
2019-01-29 10:29:20 -0800

    Add stats fixtures

--- test/stats.test.js
+++ test/stats.test.js
@@ -39,3 +39,74 @@ test('Stats - GeometryCollection Orphan', (t) => {
     t.end();
 });
 
+test('Stats - MultiLine', (t) => {
+    let res = stats({
+        input: String(path.resolve(__dirname, 'fixtures/stats.orphan-double'))
+    });
+
+    t.deepEquals(res, {
+        feats: 2,
+        clusters: 0,
+        invalid: 0,
+        addresses: 4,
+        intersections: 0,
+        address_orphans: 2,
+        network_orphans: 0
+    }, 'has 4 addresses');
+
+    t.end();
+});
+
+test('Stats - Cluster', (t) => {
+    let res = stats({
+        input: String(path.resolve(__dirname, 'fixtures/stats.cluster'))
+    });
+
+    t.deepEquals(res, {
+        feats: 1,
+        clusters: 1,
+        invalid: 0,
+        addresses: 2,
+        intersections: 0,
+        address_orphans: 0,
+        network_orphans: 0
+    }, 'has 1 cluster');
+
+    t.end();
+});
+
+test('Stats - Network Orphan', (t) => {
+    let res = stats({
+        input: String(path.resolve(__dirname, 'fixtures/stats.network'))
+    });
+
+    t.deepEquals(res, {
+        feats: 1,
+        clusters: 0,
+        invalid: 0,
+        addresses: 0,
+        intersections: 0,
+        address_orphans: 0,
+        network_orphans: 1
+    }, 'has 1 cluster');
+
+    t.end();
+});
+
+test('Stats - Invalid', (t) => {
+    let res = stats({
+        input: String(path.resolve(__dirname, 'fixtures/stats.invalid'))
+    });
+
+    t.deepEquals(res, {
+        feats: 1,
+        clusters: 0,
+        invalid: 1,
+        addresses: 0,
+        intersections: 0,
+        address_orphans: 0,
+        network_orphans: 0
+    }, 'has 1 cluster');
+
+    t.end();
+});
