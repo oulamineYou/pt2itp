2e49c762a52977fe1a1104c5825960a9e1cf671b
ingalls <ingalls@protonmail.com>
2019-01-30 07:30:57 -0800

    Create address types

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use] extern crate neon;
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde_json;
 extern crate neon_serde;
 extern crate geojson;
 extern crate postgres;
@@ -13,6 +14,60 @@ pub mod convert;
 pub mod stats;
 pub mod dedupe;
 
+/// A representation of a single Address
+pub struct Address {
+    /// An optional identifier for the address
+    id: Option<i64>,
+
+    /// The address number, can be numeric or semi-numeric (100 vs 100a)
+    number: String,
+
+    /// Vector of all street name synonyms
+    street: Vec<Name>,
+
+    /// Should the feature be output
+    output: bool,
+
+    /// Should the address feature be used to generate interpolation
+    interpolate: bool,
+
+    /// JSON representation of properties
+    props: serde_json::Value,
+
+    /// Simple representation of Lng/Lat geometry
+    geometry: Vec<(f64, f64)>
+}
+
+/// Representation of a street name with associated 
+pub struct Name {
+    /// Street Name
+    display: String,
+
+    /// When choosing which street name is primary, order by priority
+    priority: i8
+}
+
+impl Name {
+    /// Returns a representation of a street name
+    ///
+    /// # Arguments
+    ///
+    /// * `display` - A string containing the street name (Main St)
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let name = Name::new("Main St NW");
+    /// ```
+    pub fn new(display: String) -> Self {
+        Name {
+            display: display,
+            priority: 0
+        }
+    }
+}
+
+// Functions registered here will be made avaliable to be called from NodeJS
 register_module!(mut m, {
     m.export_function("convert", convert::convert)?;
     m.export_function("stats", stats::stats)?;
