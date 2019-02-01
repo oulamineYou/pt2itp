872690580f5af0f122c058bbd5d020a603f674a7
ingalls <ingalls@protonmail.com>
2019-01-31 21:10:13 -0800

    Add network table

--- native/src/pg/mod.rs
+++ native/src/pg/mod.rs
@@ -2,7 +2,7 @@ use postgres::{Connection};
 use std::io::Read;
 
 pub fn addrstream(conn: &Connection, mut data: impl Read) {
-    let stmt = conn.prepare(format!("COPY address (name, number, source, props, geom) FROM STDIN").as_str()).unwrap();
+    let stmt = conn.prepare(format!("COPY address (names, number, source, props, geom) FROM STDIN").as_str()).unwrap();
 
     stmt.copy_in(&[], &mut data).unwrap();
 }
@@ -10,6 +10,26 @@ pub fn addrstream(conn: &Connection, mut data: impl Read) {
 pub struct Table ();
 
 impl Table {
+    pub fn network(conn: &Connection) {
+        conn.execute(r#"
+             CREATE EXTENSION IF NOT EXISTS POSTGIS
+        "#, &[]).unwrap();
+
+        conn.execute(r#"
+            DROP TABLE IF EXISTS network;
+        "#, &[]).unwrap();
+
+        conn.execute(r#"
+            CREATE UNLOGGED TABLE network (
+                id SERIAL,
+                names JSONB,
+                source TEXT,
+                geom GEOMETRY(POINT, 4326),
+                props JSONB
+            )
+        "#, &[]).unwrap();
+    }
+
     pub fn address(conn: &Connection) {
         conn.execute(r#"
              CREATE EXTENSION IF NOT EXISTS POSTGIS
@@ -22,7 +42,7 @@ impl Table {
         conn.execute(r#"
             CREATE UNLOGGED TABLE address (
                 id SERIAL,
-                name JSONB,
+                names JSONB,
                 number TEXT,
                 source TEXT,
                 geom GEOMETRY(POINT, 4326),
