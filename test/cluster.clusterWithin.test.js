8ba9a02cc347e3f4d9e7e1fb53b7a7e40ffbb0d9
ingalls <ingalls@protonmail.com>
2019-02-11 10:49:57 -0800

    Passing clusterwithin tests

--- test/cluster.clusterWithin.test.js
+++ test/cluster.clusterWithin.test.js
@@ -1,6 +1,7 @@
 const Cluster = require('../lib/map/cluster');
 const Index = require('../lib/map/index');
-const init = require('../native/index.node').init;
+const pg_init = require('../native/index.node').pg_init;
+const pg_optimize = require('../native/index.node').pg_optimize;
 
 const test = require('tape');
 const fs = require('fs');
@@ -18,7 +19,7 @@ const index = new Index(pool);
 const cluster = new Cluster({ pool: pool });
 
 test('Drop/Init Database', (t) => {
-    init();
+    pg_init();
 
     index.init((err, res) => {
         t.error(err);
@@ -33,11 +34,14 @@ test('Points are clustered on netid', (t) => {
     popQ.defer((done) => {
         pool.query(`
             BEGIN;
-            INSERT INTO address (id, netid, names, number, geom) VALUES (1, 1, '[{ "tokenized": "main st", "tokenless": "main", "display": "Main Street" }]', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point","coordinates": [9.505233764648438,47.13018433161339, 1 ] }'), 4326));
-            INSERT INTO address (id, netid, names, number, geom) VALUES (2, 1, '[{ "tokenized": "main st", "tokenless": "main", "display": "Main Street" }]', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point","coordinates": [9.523429870605469,47.130797460977575, 2 ] }'), 4326));
+            INSERT INTO address (id, netid, names, number, geom) VALUES (1, 1, '[{ "tokenized": "main st", "tokenless": "main", "display": "Main Street" }]', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point","coordinates": [9.505233764648438,47.13018433161339 ] }'), 4326));
+            INSERT INTO address (id, netid, names, number, geom) VALUES (2, 1, '[{ "tokenized": "main st", "tokenless": "main", "display": "Main Street" }]', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point","coordinates": [9.523429870605469,47.130797460977575 ] }'), 4326));
             COMMIT;
         `, (err, res) => {
             t.error(err);
+            
+            pg_optimize();
+
             return done();
         });
     });
@@ -69,7 +73,7 @@ test('Points are clustered on netid', (t) => {
 });
 
 test('Drop/Init Database', (t) => {
-    init();
+    pg_init();
 
     index.init((err, res) => {
         t.error(err);
@@ -84,11 +88,14 @@ test('LineStrings far away should not be clustered', (t) => {
     popQ.defer((done) => {
         pool.query(`
             BEGIN;
-            INSERT INTO network (id, names, geom) VALUES (1, '[{ "tokenized": "main st", "tokeneless": "main", "display": "Main Street", "freq": 1 }]', ST_Multi(ST_SetSRID(ST_GeomFromGeoJSON('{"type": "LineString", "coordinates": [[9.50514793395996,47.13027192195532,1],[9.50094223022461,47.13027192195532,1]]}'), 4326)));
-            INSERT INTO network (id, names, geom) VALUES (2, '[{ "tokenized": "main st", "tokeneless": "main", "display": "Main Street", "freq": 1 }]', ST_Multi(ST_SetSRID(ST_GeomFromGeoJSON('{"type": "LineString", "coordinates": [[9.523429870605469,47.1308412556617,2],[9.527077674865723,47.13091424672175,2]]}'), 4326)));
+            INSERT INTO network (id, names, geom) VALUES (1, '[{ "tokenized": "main st", "tokeneless": "main", "display": "Main Street", "freq": 1 }]', ST_SetSRID(ST_GeomFromGeoJSON('{"type": "MultiLineString", "coordinates": [[[9.50514793395996,47.13027192195532],[9.50094223022461,47.13027192195532]]]}'), 4326));
+            INSERT INTO network (id, names, geom) VALUES (2, '[{ "tokenized": "main st", "tokeneless": "main", "display": "Main Street", "freq": 1 }]', ST_SetSRID(ST_GeomFromGeoJSON('{"type": "MultiLineString", "coordinates": [[[9.523429870605469,47.1308412556617],[9.527077674865723,47.13091424672175]]]}'), 4326));
             COMMIT;
         `, (err, res) => {
             t.error(err);
+
+            pg_optimize();
+            
             return done();
         });
     });
@@ -104,20 +111,27 @@ test('LineStrings far away should not be clustered', (t) => {
         t.error(err);
 
         pool.query(`
-            BEGIN;
-            SELECT ST_AsGeoJSON(geom)::JSON, name FROM network_cluster;
-            COMMIT;
+            SELECT
+                ST_AsGeoJSON(geom)::JSON as geom,
+                name
+            FROM
+                network_cluster
+            ORDER BY
+                id
         `, (err, res) => {
             t.error(err);
-            t.deepEquals(res[1].rows[0].st_asgeojson, { type: "MultiLineString", coordinates: [[[9.50514793395996,47.1302719219553],[9.50094223022461,47.1302719219553]]]}, 'ok network is not clustered');
-            t.deepEquals(res[1].rows[0].name, [{ freq: 1, display: 'Main Street', tokenized: 'main st', tokeneless: 'main' }]);
+            t.deepEquals(res.rows[0].geom, { type: 'MultiLineString', coordinates: [ [ [ 9.52342987060547, 47.130841255661 ], [ 9.52707767486572, 47.130914246721 ] ] ] });
+            t.deepEquals(res.rows[0].name, [{ freq: 1, display: 'Main Street', tokenized: 'main st', tokeneless: 'main' }]);
+
+            t.deepEquals(res.rows[1].geom, { type: 'MultiLineString', coordinates: [ [ [ 9.50514793395996, 47.130271921955 ], [ 9.50094223022461, 47.130271921955 ] ] ] });
+            t.deepEquals(res.rows[1].name, [{ freq: 1, display: 'Main Street', tokenized: 'main st', tokeneless: 'main' }]);
             t.end();
         });
     });
 });
 
 test('Drop/Init Database', (t) => {
-    init();
+    pg_init();
 
     index.init((err, res) => {
         t.error(err);
@@ -132,11 +146,14 @@ test('LinesStrings should be clustered', (t) => {
     popQ.defer((done) => {
         pool.query(`
             BEGIN;
-            INSERT INTO network (id, names, geom) VALUES (1, '[{ "tokenized": "main st", "tokeneless": "main", "display": "Main Street", "freq": 1 }]', ST_Multi(ST_SetSRID(ST_GeomFromGeoJSON('{"type": "LineString","coordinates": [[9.516735076904297,47.13276818606133,1],[9.519824981689451,47.132870369814995,1]]}'), 4326)));
-            INSERT INTO network (id, names, geom) VALUES (2, '[{ "tokenized": "main st", "tokeneless": "main", "display": "Main Street", "freq": 1 }]', ST_Multi(ST_SetSRID(ST_GeomFromGeoJSON('{"type": "LineString", "coordinates": [[9.513999223709106,47.132695197545665,2],[9.512518644332886,47.132695197545665,2]]},'), 4326)));
+            INSERT INTO network (id, names, geom) VALUES (1, '[{ "tokenized": "main st", "tokeneless": "main", "display": "Main Street", "freq": 1 }]', ST_SetSRID(ST_GeomFromGeoJSON('{"type": "MultiLineString","coordinates": [[[9.516735076904297,47.13276818606133],[9.519824981689451,47.132870369814995]]]}'), 4326));
+            INSERT INTO network (id, names, geom) VALUES (2, '[{ "tokenized": "main st", "tokeneless": "main", "display": "Main Street", "freq": 1 }]', ST_SetSRID(ST_GeomFromGeoJSON('{"type": "MultiLineString", "coordinates": [[[9.513999223709106,47.132695197545665],[9.512518644332886,47.132695197545665]]]},'), 4326));
             COMMIT;
         `, (err, res) => {
             t.error(err);
+
+            pg_optimize();
+
             return done();
         });
     });
@@ -152,11 +169,17 @@ test('LinesStrings should be clustered', (t) => {
         t.error(err);
 
         pool.query(`
-            SELECT ST_AsGeoJSON(geom)::JSON, name FROM network_cluster;
+            SELECT
+                ST_AsGeoJSON(geom)::JSON as geom,
+                name
+            FROM
+                network_cluster
+            ORDER BY
+                id DESC;
         `, (err, res) => {
             t.error(err);
 
-            t.deepEquals(res.rows[0].st_asgeojson, { type: "MultiLineString", coordinates: [[[9.5167350769043,47.1327681860613],[9.51982498168945,47.132870369815]],[[9.51399922370911,47.1326951975457],[9.51251864433289,47.1326951975457]]]}, 'ok network is clustered');
+            t.deepEquals(res.rows[0].geom, { type: 'MultiLineString', coordinates: [ [ [ 9.5167350769043, 47.132768186061 ], [ 9.51982498168945, 47.13287036981 ] ], [ [ 9.51399922370911, 47.132695197545 ], [ 9.51251864433289, 47.132695197545 ] ] ] });
             t.deepEquals(res.rows[0].name, [{ freq: 1, display: 'Main Street', tokenized: 'main st', tokeneless: 'main' }]);
             t.end();
         });
@@ -164,7 +187,7 @@ test('LinesStrings should be clustered', (t) => {
 });
 
 test('Drop/Init Database', (t) => {
-    init();
+    pg_init();
 
     index.init((err, res) => {
         t.error(err);
