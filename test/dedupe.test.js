7a8960ae571ddce926b99a7c8cdc4f07b99e00ae
ingalls <ingalls@protonmail.com>
2019-02-21 08:37:18 -0800

    Add hecate dedupe test framework

--- test/dedupe.test.js
+++ test/dedupe.test.js
@@ -1,10 +1,29 @@
 const test = require('tape');
 const dedupe = require('../index').dedupe;
 
-test('dedupe', (t) => {
+test('dedupe (dataset)', (t) => {
     t.doesNotThrow(() => {
         dedupe({
             db: 'test',
+            hecate: false,
+            input: 'fixtures/dedupe.geojson',
+            output: '/tmp/dedupeout.geojson',
+            tokens: 'en',
+            context: {
+                country: 'us',
+                region: 'dc'
+            }
+        });
+    });
+
+    t.end();
+});
+
+test('dedupe (hecate)', (t) => {
+    t.doesNotThrow(() => {
+        dedupe({
+            db: 'test',
+            hecate: true,
             input: 'fixtures/dedupe.geojson',
             output: '/tmp/dedupeout.geojson',
             tokens: 'en',
