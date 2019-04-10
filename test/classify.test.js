842b9f3b2893b85e2a769a75a146f555601b4d79
ingalls <ingalls@protonmail.com>
2019-04-10 13:41:55 -0400

    Update Hecate Tests to test full feature

--- test/classify.test.js
+++ test/classify.test.js
@@ -72,23 +72,128 @@ test('classify (hecate)', (t) => {
 
     t.deepEquals(output[0], undefined);
 
-    t.deepEquals(output[1].properties.accuracy, output[1].properties.expected);
+    t.deepEquals(output[1], {
+        id: 1,
+        version: 0,
+        type: 'Feature',
+        action: 'modify',
+        properties: {
+            accuracy: 'rooftop',
+            expected: 'rooftop',
+            number: 1,
+            street: 'Main St'
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [ -79.3766257166862, 38.8341710657445 ]
+        }
+    });
 
     t.deepEquals(output[2], undefined);
 
-    t.deepEquals(output[3].properties.accuracy, output[3].properties.expected);
+    t.deepEquals(output[3], {
+        id: 3,
+        version: 0,
+        type: 'Feature',
+        action: 'modify',
+        properties: {
+            accuracy: 'rooftop',
+            expected: 'rooftop',
+            number: 3,
+            street: 'Main St'
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [ -79.3770414590836, 38.8339495948735 ]
+        }
+    });
 
-    t.deepEquals(output[4].properties.accuracy, output[4].properties.expected);
+    t.deepEquals(output[4], {
+        id: 4,
+        version: 0,
+        type: 'Feature',
+        action: 'modify',
+        properties: {
+            accuracy: 'rooftop',
+            expected: 'rooftop',
+            number: 4,
+            street: 'Main St'
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [ -79.3768778443336, 38.8344489480323 ]
+        }
+    });
 
-    t.deepEquals(output[5].properties.accuracy, output[5].properties.expected);
+    t.deepEquals(output[5], {
+        id: 5,
+        version: 0,
+        type: 'Feature',
+        action: 'modify',
+        properties: {
+            accuracy: 'rooftop',
+            expected: 'rooftop',
+            number: 5,
+            street: 'Main St'
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [ -79.3768563866615, 38.8343779105582 ]
+        }
+    });
 
     t.deepEquals(output[6], undefined);
 
-    t.deepEquals(output[7].properties.accuracy, output[7].properties.expected);
+    t.deepEquals(output[7], {
+        id: 7,
+        version: 0,
+        type: 'Feature',
+        action: 'modify',
+        properties: {
+            accuracy: 'parcel',
+            expected: 'parcel',
+            number: 7,
+            street: 'Main St'
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [ -79.3765304982662, 38.8336497715408 ]
+        }
+    });
 
-    t.deepEquals(output[8].properties.accuracy, output[8].properties.expected);
+    t.deepEquals(output[8], {
+        id: 8,
+        version: 0,
+        type: 'Feature',
+        action: 'modify',
+        properties: {
+            accuracy: 'point',
+            expected: 'point',
+            number: 8,
+            street: 'Main St'
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [ -79.4051456451416, 38.8391059265703 ]
+        }
+    });
 
-    t.deepEquals(output[9].properties.accuracy, output[9].properties.expected);
+    t.deepEquals(output[9], {
+        id: 9,
+        version: 0,
+        type: 'Feature',
+        action: 'modify',
+        properties: {
+            accuracy: 'point',
+            expected: 'point',
+            number: 9,
+            street: 'Main St'
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [ -79.4054889678955, 38.8344259653278 ]
+        }
+    });
 
     t.end();
 });
