576a90cb29c26ac6e6f935235370a69552b4b77c
Lizzie Gooding <lizzie.gooding@mapbox.com>
2019-05-22 10:26:31 -0400

    revert variable/parameter renaming in conflate

--- native/src/util/linker.rs
+++ native/src/util/linker.rs
@@ -5,6 +5,7 @@ use crate::text::{
 };
 
 use crate::types::Names;
+use geocoder_abbreviations::TokenType;
 
 pub struct Link<'a> {
     pub id: i64,
@@ -50,7 +51,6 @@ pub fn linker(primary: Link, mut potentials: Vec<Link>, strict: bool) -> Option<
             'outer: for potential_name in &potential.names.names {
 
                 // Ensure exact matches are always returned before potential short-circuits
-                // make sure this actually works with objects
                 if name.tokenized == potential_name.tokenized {
                     return Some(LinkResult::new(potential.id, 100.0));
                 }
@@ -68,7 +68,7 @@ pub fn linker(primary: Link, mut potentials: Vec<Link>, strict: bool) -> Option<
                                     continue 'outer;
                                 }
                             },
-                            Some(_) | None => ()
+                            _ => ()
                         }
                     }
                 }
@@ -563,6 +563,15 @@ mod tests {
             let a = Link::new(1, &a_name);
             let b = vec![Link::new(2, &b_name)];
             assert_eq!(linker(a, b, true), Some(LinkResult::new(2, 90.0)));
+        }
+
+        {
+            let a_name = Names::new(vec![Name::new("Main", 0, &context)], &context);
+            let b_name = Names::new(vec![Name::new("Main Street", 0, &context)], &context);
+
+            let a = Link::new(1, &a_name);
+            let b = vec![Link::new(2, &b_name)];
+            assert_eq!(linker(a, b, true), Some(LinkResult::new(2, 86.36)));
         }
 
         {
